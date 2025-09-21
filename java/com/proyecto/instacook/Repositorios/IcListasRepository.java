4a532cd228a365fe43c84fe848faff6938d123e6
LARA MILLÁN TROYA <laramillant13@gmail.com>
2025-09-21 13:11:41 +0200

    Delete java/com/proyecto/instacook directory

--- java/com/proyecto/instacook/Repositorios/IcListasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.instacook.Repositorios;
-
-import com.proyecto.instacook.Entidades.IcListas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IcListasRepository extends JpaRepository <IcListas,Integer> {
-}
